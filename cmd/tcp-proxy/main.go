35d88e30cd490d365dcf0d719ae352c85fdd743a
Samit <12378746+samitpal@users.noreply.github.com>
2022-12-13 19:19:25 +0530

    Tee'ing tcp proxy with 2 backends. The proxy takes two remote backends and sends all tcp traffic from clients to both the remote backends. //The proxy sends the response from the first remote back to the client and discards the one received from the second remote

--- cmd/tcp-proxy/main.go
+++ cmd/tcp-proxy/main.go
@@ -1,3 +1,5 @@
+//A tee'ing tcp proxy. The proxy takes two remote backends and sends all tcp traffic from clients to both the remote backends.
+//The proxy sends the response from the first remote back to the client and discards the one received from the second remote
 package main
 
 import (
@@ -5,28 +7,23 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"regexp"
-	"strings"
 
 	proxy "github.com/jpillora/go-tcp-proxy"
 )
 
 var (
-	version = "0.0.0-src"
-	matchid = uint64(0)
+	version = "0.0.1"
 	connid  = uint64(0)
 	logger  proxy.ColorLogger
 
-	localAddr   = flag.String("l", ":9999", "local address")
-	remoteAddr  = flag.String("r", "localhost:80", "remote address")
-	verbose     = flag.Bool("v", false, "display server actions")
-	veryverbose = flag.Bool("vv", false, "display server actions and all tcp data")
-	nagles      = flag.Bool("n", false, "disable nagles algorithm")
-	hex         = flag.Bool("h", false, "output hex")
-	colors      = flag.Bool("c", false, "output ansi colors")
-	unwrapTLS   = flag.Bool("unwrap-tls", false, "remote connection with TLS exposed unencrypted locally")
-	match       = flag.String("match", "", "match regex (in the form 'regex')")
-	replace     = flag.String("replace", "", "replace regex (in the form 'regex~replacer')")
+	localAddr        = flag.String("l", ":9999", "local address")
+	remoteAddr       = flag.String("r1", "localhost:80", "first remote address. The client gets response back only from the first remote.")
+	secondRemoteAddr = flag.String("r2", "localhost:8080", "second remote address. The proxy discards the response from this remote")
+	verbose          = flag.Bool("v", false, "display server actions")
+	veryverbose      = flag.Bool("vv", false, "display server actions and all tcp data")
+	nagles           = flag.Bool("n", false, "disable nagles algorithm")
+	hex              = flag.Bool("h", false, "output hex")
+	colors           = flag.Bool("c", false, "output ansi colors")
 )
 
 func main() {
@@ -37,16 +34,21 @@ func main() {
 		Color:   *colors,
 	}
 
-	logger.Info("go-tcp-proxy (%s) proxing from %v to %v ", version, *localAddr, *remoteAddr)
+	logger.Info("go-tcp-proxy (%s) proxing from %v to %v and %v", version, *localAddr, *remoteAddr, *secondRemoteAddr)
 
 	laddr, err := net.ResolveTCPAddr("tcp", *localAddr)
 	if err != nil {
 		logger.Warn("Failed to resolve local address: %s", err)
 		os.Exit(1)
 	}
-	raddr, err := net.ResolveTCPAddr("tcp", *remoteAddr)
+	r1addr, err := net.ResolveTCPAddr("tcp", *remoteAddr)
 	if err != nil {
 		logger.Warn("Failed to resolve remote address: %s", err)
+		os.Exit(1)
+	}
+	r2addr, err := net.ResolveTCPAddr("tcp", *secondRemoteAddr)
+	if err != nil {
+		logger.Warn("Failed to resolve second remote address: %s", err)
 		os.Exit(1)
 	}
 	listener, err := net.ListenTCP("tcp", laddr)
@@ -54,9 +56,6 @@ func main() {
 		logger.Warn("Failed to open local port to listen: %s", err)
 		os.Exit(1)
 	}
-
-	matcher := createMatcher(*match)
-	replacer := createReplacer(*replace)
 
 	if *veryverbose {
 		*verbose = true
@@ -71,15 +70,7 @@ func main() {
 		connid++
 
 		var p *proxy.Proxy
-		if *unwrapTLS {
-			logger.Info("Unwrapping TLS")
-			p = proxy.NewTLSUnwrapped(conn, laddr, raddr, *remoteAddr)
-		} else {
-			p = proxy.New(conn, laddr, raddr)
-		}
-
-		p.Matcher = matcher
-		p.Replacer = replacer
+		p = proxy.New(conn, laddr, r1addr, r2addr)
 
 		p.Nagles = *nagles
 		p.OutputHex = *hex
@@ -91,50 +82,5 @@ func main() {
 		}
 
 		go p.Start()
-	}
-}
-
-func createMatcher(match string) func([]byte) {
-	if match == "" {
-		return nil
-	}
-	re, err := regexp.Compile(match)
-	if err != nil {
-		logger.Warn("Invalid match regex: %s", err)
-		return nil
-	}
-
-	logger.Info("Matching %s", re.String())
-	return func(input []byte) {
-		ms := re.FindAll(input, -1)
-		for _, m := range ms {
-			matchid++
-			logger.Info("Match #%d: %s", matchid, string(m))
-		}
-	}
-}
-
-func createReplacer(replace string) func([]byte) []byte {
-	if replace == "" {
-		return nil
-	}
-	//split by / (TODO: allow slash escapes)
-	parts := strings.Split(replace, "~")
-	if len(parts) != 2 {
-		logger.Warn("Invalid replace option")
-		return nil
-	}
-
-	re, err := regexp.Compile(string(parts[0]))
-	if err != nil {
-		logger.Warn("Invalid replace regex: %s", err)
-		return nil
-	}
-
-	repl := []byte(parts[1])
-
-	logger.Info("Replacing %s with %s", re.String(), repl)
-	return func(input []byte) []byte {
-		return re.ReplaceAll(input, repl)
 	}
 }
--- proxy.go
+++ proxy.go
@@ -1,25 +1,20 @@
 package proxy
 
 import (
-	"crypto/tls"
 	"io"
 	"net"
 )
 
 // Proxy - Manages a Proxy connection, piping data between local and remote.
 type Proxy struct {
-	sentBytes     uint64
-	receivedBytes uint64
-	laddr, raddr  *net.TCPAddr
-	lconn, rconn  io.ReadWriteCloser
-	erred         bool
-	errsig        chan bool
-	tlsUnwrapp    bool
-	tlsAddress    string
-
-	Matcher  func([]byte)
-	Replacer func([]byte) []byte
-
+	sentBytes             uint64
+	receivedBytes         uint64
+	laddr, r1addr, r2addr *net.TCPAddr
+	lconn, r1conn, r2conn io.ReadWriteCloser
+	erred                 bool
+	errsig                chan bool
+	// this is to hold a copy of the local socket data to be sent to second remote
+	buffSecondRemote chan []byte
 	// Settings
 	Nagles    bool
 	Log       Logger
@@ -28,25 +23,17 @@ type Proxy struct {
 
 // New - Create a new Proxy instance. Takes over local connection passed in,
 // and closes it when finished.
-func New(lconn *net.TCPConn, laddr, raddr *net.TCPAddr) *Proxy {
+func New(lconn *net.TCPConn, laddr, r1addr *net.TCPAddr, r2addr *net.TCPAddr) *Proxy {
 	return &Proxy{
-		lconn:  lconn,
-		laddr:  laddr,
-		raddr:  raddr,
-		erred:  false,
-		errsig: make(chan bool),
-		Log:    NullLogger{},
+		lconn:            lconn,
+		laddr:            laddr,
+		r1addr:           r1addr,
+		r2addr:           r2addr,
+		erred:            false,
+		errsig:           make(chan bool),
+		buffSecondRemote: make(chan []byte, 0xffff),
+		Log:              NullLogger{},
 	}
-}
-
-// NewTLSUnwrapped - Create a new Proxy instance with a remote TLS server for
-// which we want to unwrap the TLS to be able to connect without encryption
-// locally
-func NewTLSUnwrapped(lconn *net.TCPConn, laddr, raddr *net.TCPAddr, addr string) *Proxy {
-	p := New(lconn, laddr, raddr)
-	p.tlsUnwrapp = true
-	p.tlsAddress = addr
-	return p
 }
 
 type setNoDelayer interface {
@@ -58,34 +45,46 @@ func (p *Proxy) Start() {
 	defer p.lconn.Close()
 
 	var err error
-	//connect to remote
-	if p.tlsUnwrapp {
-		p.rconn, err = tls.Dial("tcp", p.tlsAddress, nil)
-	} else {
-		p.rconn, err = net.DialTCP("tcp", nil, p.raddr)
-	}
+	//connect to first remote
+	p.r1conn, err = net.DialTCP("tcp", nil, p.r1addr)
 	if err != nil {
 		p.Log.Warn("Remote connection failed: %s", err)
 		return
 	}
-	defer p.rconn.Close()
+	defer p.r1conn.Close()
 
+	//connect to second remote
+	p.r2conn, err = net.DialTCP("tcp", nil, p.r2addr)
+	if err != nil {
+		p.Log.Warn("Second Remote connection failed: %s", err)
+		return
+	}
+	defer p.r2conn.Close()
+	
 	//nagles?
 	if p.Nagles {
 		if conn, ok := p.lconn.(setNoDelayer); ok {
 			conn.SetNoDelay(true)
 		}
-		if conn, ok := p.rconn.(setNoDelayer); ok {
+		if conn, ok := p.r1conn.(setNoDelayer); ok {
+			conn.SetNoDelay(true)
+		}
+		if conn, ok := p.r2conn.(setNoDelayer); ok {
 			conn.SetNoDelay(true)
 		}
 	}
 
 	//display both ends
-	p.Log.Info("Opened %s >>> %s", p.laddr.String(), p.raddr.String())
+	p.Log.Info("Opened %s >>> %s", p.laddr.String(), p.r1addr.String())
 
-	//bidirectional copy
-	go p.pipe(p.lconn, p.rconn)
-	go p.pipe(p.rconn, p.lconn)
+	//bidirectional copy to first remote
+	go p.pipe(p.lconn, p.r1conn)
+	go p.pipe(p.r1conn, p.lconn)
+
+	// close the buffered channel meant for second remote
+	defer close(p.buffSecondRemote)
+	// one way copy to second remote
+	go p.pipeToSecondRemote(p.r2conn)
 
 	//wait for close...
 	<-p.errsig
@@ -130,20 +129,17 @@ func (p *Proxy) pipe(src, dst io.ReadWriter) {
 		}
 		b := buff[:n]
 
-		//execute match
-		if p.Matcher != nil {
-			p.Matcher(b)
-		}
-
-		//execute replace
-		if p.Replacer != nil {
-			b = p.Replacer(b)
-		}
-
 		//show output
 		p.Log.Debug(dataDirection, n, "")
 		p.Log.Trace(byteFormat, b)
 
+		// send a copy of the socket data to the channel meant for the second remote
+		go func() {
+			if islocal {
+				buff1 := buff[:n]
+				p.buffSecondRemote <- buff1
+			}
+		}()
 		//write out result
 		n, err = dst.Write(b)
 		if err != nil {
@@ -154,6 +150,16 @@ func (p *Proxy) pipe(src, dst io.ReadWriter) {
 			p.sentBytes += uint64(n)
 		} else {
 			p.receivedBytes += uint64(n)
+		}
+	}
+}
+
+func (p *Proxy) pipeToSecondRemote(dst io.ReadWriter) {
+	for d := range p.buffSecondRemote {
+		_, err := dst.Write(d)
+		if err != nil {
+			p.err("Write failed to second remote '%s'\n", err)
+			return
 		}
 	}
 }
